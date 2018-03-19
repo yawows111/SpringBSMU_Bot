93e1e0b09b5dc053622a3718111d8a73bb1171b4
yawows111 <yawows111@gmail.com>
2018-03-19 18:15:52 +0500

    first commit

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 import telebot
 
 
+import os
+from flask import Flask, request
+import logging
+
+
 
 
 bot = telebot.TeleBot('589935777:AAHYS__Ii5o90oKbNJWoF9oeilBTbruKh2g')
@@ -16,6 +21,27 @@ def handle_start(message):
     user_markup.row('Текущие состояние голосования!')
     user_markup.row('Информация о Боте.')
 
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://springbsmu-bot.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
 
 bot.polling(none_stop=True)
 
